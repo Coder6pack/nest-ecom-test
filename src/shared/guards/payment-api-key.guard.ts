4702d67eb5fbb22e8cd0e6aab1dbf9981f2bae88
Coder6pack <hoainhonlam10@gmail.com>
2025-05-17 10:19:05 +0700

    handle paymen with socket.io

--- src/shared/guards/payment-api-key.guard.ts
+++ src/shared/guards/payment-api-key.guard.ts
@@ -5,7 +5,8 @@ import envConfig from 'src/shared/config'
 export class PaymentAPIKeyGuard implements CanActivate {
 	canActivate(context: ExecutionContext): boolean {
 		const request = context.switchToHttp().getRequest()
-		const paymentApiKey = request.headers['payment-api-key']
+		console.log(request.headers['authorization']?.split(' ')[1])
+		const paymentApiKey = request.headers['authorization']?.split(' ')[1]
 		if (paymentApiKey !== envConfig.PAYMENT_API_KEY) {
 			throw new UnauthorizedException()
 		}
--- src/websockets/websocket.adapter.ts
+++ src/websockets/websocket.adapter.ts
@@ -1,7 +1,18 @@
+import { INestApplicationContext } from '@nestjs/common'
 import { IoAdapter } from '@nestjs/platform-socket.io'
-import { ServerOptions, Server } from 'socket.io'
+import { ServerOptions, Server, Socket } from 'socket.io'
+import { generateRoomUserId } from 'src/shared/helpers'
+import { SharedWebsocketRepository } from 'src/shared/repositories/shared-websocket.repo'
+import { TokenService } from 'src/shared/services/token.service'
 
 export class WebSocketAdapter extends IoAdapter {
+	private readonly sharedWebSocketRepository: SharedWebsocketRepository
+	private readonly tokenService: TokenService
+	constructor(app: INestApplicationContext) {
+		super(app)
+		this.sharedWebSocketRepository = app.get(SharedWebsocketRepository)
+		this.tokenService = app.get(TokenService)
+	}
 	createIOServer(port: number, options?: ServerOptions) {
 		const server: Server = super.createIOServer(3003, {
 			...options,
@@ -10,6 +21,41 @@ export class WebSocketAdapter extends IoAdapter {
 				credential: true,
 			},
 		})
+		server.use((socket, next) => {
+			this.authMiddleware(socket, next)
+				.then()
+				.catch((err) => console.log(err))
+		})
+		server.of(/.*/).use((socket, next) => {
+			this.authMiddleware(socket, next)
+				.then()
+				.catch((err) => console.log(err))
+		})
 		return server
+	}
+	async authMiddleware(socket: Socket, next: (err?: any) => void) {
+		const { authorization } = socket.handshake.headers
+		if (!authorization) {
+			return next(new Error('No Authorization'))
+		}
+		const accessToken = authorization.split(' ')[1]
+		if (!accessToken) {
+			return next(new Error('No AccessToken'))
+		}
+		try {
+			const { userId } = await this.tokenService.verifyAccessToken(accessToken)
+			await socket.join(generateRoomUserId(userId))
+			// await this.sharedWebSocketRepository.create({
+			// 	id: socket.id,
+			// 	userId,
+			// })
+			// socket.on('disconnect', () => {
+			// 	console.log('disconnect:', socket.id)
+			// })
+			next()
+		} catch (error) {
+			next(error)
+		}
+		console.log('connection', socket.id)
 	}
 }
